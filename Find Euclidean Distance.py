df253714983cc22593fba88909b7c3f70ea0e863
Chandrakant Thakur <69152112+Chandrakant817@users.noreply.github.com>
2023-01-18 23:14:59 +0530

    Add files via upload

---
+++ Find Euclidean Distance.py
@@ -0,0 +1,9 @@
+# Find Euclidean Distance
+
+x1 = float(input("Enter the Number x1: ")) 
+x2 = float(input("Enter the Number x2: "))
+y1 = float(input("Enter the Number y1: "))
+y2 = float(input("Enter the Number y2: "))
+
+res = ((x2-x1)**2 + (y2-y1)**2)**0.5
+print(res)
