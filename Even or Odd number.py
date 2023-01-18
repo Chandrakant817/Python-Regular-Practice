615e31d5f0779249ff4b84cddb804d919286fbec
Chandrakant Thakur <69152112+Chandrakant817@users.noreply.github.com>
2023-01-18 22:52:34 +0530

    Add files via upload

---
+++ Even or Odd number.py
@@ -0,0 +1,19 @@
+# Program to check Even or Odd
+
+x = int(input("Enter the Number: "))
+
+if x%2 == 0:
+  print("Even Number")
+else:
+  print("Odd Number")
+  
+"""
+def check(num):
+  if num%2==0:
+    print("Even")
+  else:
+    print("Odd")
+
+x = int(input())
+check(x)
+"""
