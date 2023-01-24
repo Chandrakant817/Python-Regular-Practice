425a739e2725ee6d6d0ee8163898db0670ab4984
Chandrakant Thakur <69152112+Chandrakant817@users.noreply.github.com>
2023-01-24 20:42:29 +0530

    Update Even or Odd number.py

--- Even or Odd number.py
+++ Even or Odd number.py
@@ -17,3 +17,17 @@ def check(num):
 x = int(input())
 check(x)
 """
+
+
+"""
+# Check for Even or Odd using While Loop
+
+n = int(input("Enter the Number"))
+while n > 0:
+  if n%2 == 0:
+    print(n, "is a Even Number")
+    break
+  else:
+    print(n, "is a Odd Number")
+    break
+"""
