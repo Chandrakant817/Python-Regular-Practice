67f1209fb758209b6d7a9e02d8af139a1f6f3539
Chandrakant Thakur <69152112+Chandrakant817@users.noreply.github.com>
2024-04-15 18:40:10 +0530

    Update Mock_interview.py

--- Mock_interview.py
+++ Mock_interview.py
@@ -1,6 +1,33 @@
 """input: simple strings are simple
 output: s,e,4
 """
+x = input("Enter a string: ")
+
+counts = {}
+
+for i in x:
+    if i in counts:
+        counts[i] += 1
+    else:
+        counts[i] = 1
+print(counts)
+
+# Find the maximum occurrence count
+max_count = max(counts.values())  
+print(max_count)
+
+max_chars = []
+for key, value in counts.items():
+    if value == max_count:
+        max_chars.append(key)
+
+# Displaying the result
+result = ",".join(max_chars) + "," + str(max_count)
+print(f"The character '{','.join(max_chars)}' occurred {max_count} time the most.")
+print(result)
+
+---------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------
 
 input_string = input("Enter a string: ")
 
